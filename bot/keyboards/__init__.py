e28a4aa87cc02c411f77575d0628ffabc2cf7dfc
Vadym <vadymkravtcsov@gmail.com>
2026-02-17 23:23:03 +0200

    add: files __init__ (for convenient  module imports)

---
+++ bot/keyboards/__init__.py
@@ -0,0 +1,8 @@
+"""
+This module contains the keyboard definitions for the Telegram bot.
+To export a module not through the project root
+"""
+
+from .repl import key_city
+
+__all__ = ["key_city"]
---
+++ modules/__init__.py
@@ -0,0 +1,8 @@
+"""
+This module contains the keyboard definitions for the Telegram bot.
+To export a module not through the project root
+"""
+
+from .weather_api import get_weather
+
+__all__ = ["get_weather"]
